82f86e39eac62632443821c6aba05154ec20b21d
ishan115 <ali3nator5000@gmail.com>
2019-03-13 10:09:33 -0500

    Start Project

---
+++ Assets/FungusExamples/FungusLua/Bindings/CustomScript.cs
@@ -0,0 +1,35 @@
+// This code is part of the Fungus library (http://fungusgames.com) maintained by Chris Gregan (http://twitter.com/gofungus).
+// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomScript : MonoBehaviour 
+{
+    public float panSpeed = 20f;
+    public float speed = 1.5f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
